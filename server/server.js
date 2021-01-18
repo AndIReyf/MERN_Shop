23f8c42b98b1a7866ccd9861b1fdf9774c1da210
AndIReyf <andrey.prudius@hotmail.com>
2021-01-18 20:28:38 +0100

    feat: add mongoDB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 app.use(cors())
